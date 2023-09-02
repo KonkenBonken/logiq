9bfc12c0c31a83b18912b6da72efd21a6c2ca4ca
KonkenBonken <konrad05pettersson@gmail.com>
2023-09-02 15:46:26 +0200

    Added gate assets

--- src/consts.ts
+++ src/consts.ts
@@ -11,9 +11,14 @@ export interface Tile {
 }
 
 export class Component {
+  readonly imgSrc: string;
+
   constructor(
+    readonly type: ComponentTypes,
     readonly computer: (...input: boolean[]) => boolean
-  ) { }
+  ) {
+    this.imgSrc = `/gates/${this.type}.webp`;
+  }
 
   get inputCount() {
     return this.computer.length;
@@ -21,6 +26,6 @@ export class Component {
 }
 
 export const components = {
-  or: new Component((a, b) => a || b),
-  and: new Component((a, b) => a && b),
+  or: new Component('or', (a, b) => a || b),
+  and: new Component('and', (a, b) => a && b),
 } satisfies Record<ComponentTypes, Component>;
