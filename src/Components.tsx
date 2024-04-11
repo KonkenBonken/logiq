d1d6ce2146b651e93a2439b857b56fc971e6ac1a
KonkenBonken <konrad05pettersson@gmail.com>
2024-04-11 19:40:24 +0200

    click outside to clear selected component

--- src/Components.tsx
+++ src/Components.tsx
@@ -7,20 +7,22 @@ import TileComponent from './TileComponent';
 export default function Components(
   { selected: [selected, setSelected] }: { selected: ReturnType<typeof useState<TileTypes | undefined>> }
 ) {
-  return <div id="components" >
+  return <div id="components" onClick={() => {
+    setSelected(undefined);
+  }}>
     {(Object.entries(components) as [TileTypes, Component][])
       .map(([type, component]) =>
         <div
           key={type}
           className={clsx({ selected: selected === type })}
-          onClick={() => setSelected(type)}
+          onClick={() => setTimeout(() => setSelected(type))}
         >
           <TileComponent tile={{ type }} />
         </div>
       )}
     <div
       className={clsx({ selected: selected === 'wire' })}
-      onClick={() => setSelected('wire')}
+      onClick={() => setTimeout(() => setSelected('wire'))}
     >
       <TileComponent tile={{ type: 'wire' }} />
     </div>
